3fcade7374c968063773523a66e1df4125c53d0f
muresame <lynx7478@gmail.com>
2017-07-17 16:13:20 +0200

    Add CAD class.

---
+++ src/com/gmail/lynx7478/mahou/CAD.java
@@ -0,0 +1,64 @@
+package com.gmail.lynx7478.mahou;
+
+import org.bukkit.inventory.ItemStack;
+
+public class CAD 
+{
+	
+	private int levelRequired;
+	private int cadLevel;
+	private int baseLevel;
+	private double basePower;
+	private double powerPerLevel;
+	private double maxPower;
+	
+	private String name;
+	private String codeName;
+	private ItemStack item;
+	
+	public CAD(String name, String codeName, int levelRequired, int cadLevel, int level, double basePower, double powerPerLevel,
+			double maxPower, ItemStack item)
+	{
+		this.levelRequired = levelRequired;
+		this.cadLevel = cadLevel;
+		this.baseLevel = baseLevel;
+		this.basePower = basePower;
+		this.powerPerLevel = powerPerLevel;
+		this.maxPower = maxPower;
+		
+		this.name = name;
+		this.codeName = codeName;
+		this.item = item;
+	}
+	
+	public int getLevelRequired()
+	{
+		return levelRequired;
+	}
+	
+	public int getCADLevel()
+	{
+		return cadLevel;
+	}
+	
+	public int getBaseLevel()
+	{
+		return baseLevel;
+	}
+	
+	public double getBasePower()
+	{
+		return basePower;
+	}
+	
+	public double getPowerPerLevel()
+	{
+		return powerPerLevel;
+	}
+	
+	public double getMaxPower()
+	{
+		return maxPower;
+	}
+
+}
