e55f30789ae1a0f4288a2e7a17ea31fa03a8b4b0
muresame <lynx7478@gmail.com>
2017-07-17 17:35:19 +0200

    Add MahouPlayer.

---
+++ src/com/gmail/lynx7478/mahou/player/MahouPlayer.java
@@ -0,0 +1,22 @@
+package com.gmail.lynx7478.mahou.player;
+
+import org.bukkit.entity.Player;
+
+import com.gmail.lynx7478.mahou.CAD;
+
+public class MahouPlayer 
+{
+	
+	private Player bukkitPlayer;
+	private String name;
+	private int level;
+	private double power;
+	private double basePower;
+	private CAD[] cads;
+	
+	public MahouPlayer()
+	{
+		
+	}
+
+}
