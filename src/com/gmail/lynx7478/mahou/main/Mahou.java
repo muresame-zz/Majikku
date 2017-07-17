b75549d24542471ae5648c84717f811f6a805650
muresame <lynx7478@gmail.com>
2017-07-17 16:48:13 +0200

    Added to intilize stuff.

--- src/com/gmail/lynx7478/mahou/main/Mahou.java
+++ src/com/gmail/lynx7478/mahou/main/Mahou.java
@@ -2,6 +2,9 @@ package com.gmail.lynx7478.mahou.main;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.gmail.lynx7478.mahou.CADManager;
+import com.gmail.lynx7478.mahou.SpellManager;
+
 public class Mahou extends JavaPlugin 
 {
 	
@@ -12,12 +15,29 @@ public class Mahou extends JavaPlugin
 		return instance;
 	}
 	
+	private CADManager cadManager;
+	private SpellManager spellManager;
+	
 	@Override
 	public void onEnable()
 	{
 		instance = this;
 		
+		// Initialize the CAD manager.
+		cadManager = new CADManager();
 		
+		// Initilize the Spell manager.
+		spellManager = new SpellManager();
+	}
+	
+	public CADManager getCADManager()
+	{
+		return cadManager;
+	}
+	
+	public SpellManager getSpellManager()
+	{
+		return spellManager;
 	}
 
 }
