b54b74f4252397f1d00e9753a344925ccc58ed66
muresame <lynx7478@gmail.com>
2017-07-17 15:34:41 +0200

    Add Spell class.

---
+++ src/com/gmail/lynx7478/mahou/Spell.java
@@ -0,0 +1,51 @@
+package com.gmail.lynx7478.mahou;
+
+public class Spell 
+{
+	
+	private double castTime;
+	private String name;
+	private String codeName;
+//	private CAD[] cads;
+	
+	public Spell(String name, String codeName, double castTime)
+	{
+		this.castTime = castTime;
+		this.name = name;
+		this.codeName = name;
+	}
+	
+	/* public Spell(String name, String codeName, double castTime, CAD[] cads)
+	{
+		this.castTime = castTime;
+		this.name = name;
+		this.codeName = name;
+		this.cads = cads;
+	} */
+	
+	public void onCast(){}
+	
+	public void preCast(){}
+	
+	public void duringCast() {}
+	
+	public void spell() {}
+	
+	public void onEnd() {}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getCodename()
+	{
+		return codeName;
+	}
+	
+	public double getCastTime()
+	{
+		return castTime;
+	}
+
+}
