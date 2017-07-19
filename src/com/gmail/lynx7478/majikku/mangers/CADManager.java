8fd0f9670548accaa1abde49ee117f377567254c
muresame <lynx7478@gmail.com>
2017-07-19 18:41:15 +0200

    bada bing bada boom x2

---
+++ src/com/gmail/lynx7478/majikku/mangers/CADManager.java
@@ -0,0 +1,49 @@
+package com.gmail.lynx7478.majikku.mangers;
+
+import java.util.ArrayList;
+
+import com.gmail.lynx7478.majikku.CAD;
+
+public class CADManager 
+{
+	private ArrayList<CAD> cads;
+	
+	public CADManager()
+	{
+		cads = new ArrayList<CAD>();
+		
+		
+	}
+	
+	public ArrayList<CAD> getCADs()
+	{
+		return cads;
+	}
+	
+	public CAD getCAD(Class<? extends CAD> clazz)
+	{
+		for(CAD c : cads)
+		{
+			if(c.getClass() == clazz)
+			{
+				return c;
+			}
+		}
+		return null;
+	}
+	
+	public CAD getCADByName(String str)
+	{
+		str = str.replaceAll(" ", "");
+		for(final CAD c : cads)
+		{
+			if(c.getCodename().replaceAll(" ", "").equalsIgnoreCase(str))
+			{
+				return c;
+			}
+		}
+		return null;
+}
+
+
+}
